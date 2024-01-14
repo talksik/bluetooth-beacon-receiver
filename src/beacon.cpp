a5e51b0bc69e72e5be2d168e552a51c49103b611
Arjun Patel <patel.arjun50@gmail.com>
2024-01-14 09:08:48 -0800

    adding in basic meson building of the two output executables

---
+++ src/beacon.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  printf("Hello world, I am the beacon\n");
+  return 0;
+}
---
+++ src/receiver.cpp
@@ -0,0 +1,7 @@
+
+#include <cstdio>
+
+int main(int argc, char *argv[]) {
+  printf("Hello World, I am the receiver\n");
+  return 0;
+}
