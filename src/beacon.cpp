3b591a3a36233d3108a7abdbbfe0e2db13325250
Arjun Patel <patel.arjun50@gmail.com>
2024-01-14 10:10:40 -0800

    linking with bluetooth library although compiled without linking it because not actually using any methods from the headers

--- src/beacon.cpp
+++ src/beacon.cpp
@@ -1,6 +1,10 @@
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/hci.h>
 #include <stdio.h>
+#include <bluetooth/hci_lib.h>
 
 int main(int argc, char *argv[]) {
   printf("Hello world, I am the beacon\n");
+
   return 0;
 }
